a5df09b43eabb846b624cea45b0ac3112804a5cc
Gabrieleabu23 <kaidgabriele23@gmail.com>
2023-11-23 01:32:01 +0100

    style: added some styles layover history, divided score user from pc

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,15 +1,25 @@
-
+let score;
+let i=0;
 function invio(){
-    let userchoice= Math.floor(Math.random()*6 + 1 );
-    let pcChoise= Math.floor(Math.random()*6 + 1 );
-
+    document.getElementById("risultato-user").innerHTML=" ";
+    document.getElementById("risultato-pc").innerHTML=" ";
+    let userchoice= Math.floor(Math.random() * 6 + 1 );
+    let pcChoise= Math.floor(Math.random() * 6 + 1 );
+    document.getElementById("risultato-user").append(`${"Utente: "} ${userchoice}`);
+    document.getElementById("risultato-pc").append(`${"Computer: "} ${pcChoise}`);
     if(userchoice > pcChoise){
-        document.getElementById("risultato").innerHTML= `${userchoice} ${"hai vinto"} ${pcChoise}`;
+        document.getElementById("risultato").innerHTML= "Hai Vinto!";
+        score="Hai Vinto!";
     } else if (userchoice === pcChoise){
-        document.getElementById("risultato").innerHTML="pareggio!";
-        document.getElementById("risultato").innerHTML= `${userchoice} ${"pareggio"} ${pcChoise}`;
+        document.getElementById("risultato").innerHTML="Pari!";
+        score="Pari!";
     }
     else{
-        document.getElementById("risultato").innerHTML= `${userchoice} ${"hai perso"} ${pcChoise}`;
+        document.getElementById("risultato").innerHTML= "Hai Perso!";
+        score="Hai Perso!";
     }
+    const history= document.createElement("div");
+    i+=1;
+    history.append(`${i}${"° Partita"} ${"(Utente: "} ${userchoice} ${score} ${"Computer: "} ${pcChoise}${")"}`);
+    document.querySelector(".cronologia").append(history);
 }
